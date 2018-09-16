8aea74a0c7c9deb24074db4de207aa4559033a7e
vdrake6 <vdrake6@gmail.com>
2018-09-17 00:42:45 +0200

    JsonTokenizer refactor #2

--- src/main/scala/nullpointer/json/JsonTokenizer.scala
+++ src/main/scala/nullpointer/json/JsonTokenizer.scala
@@ -3,62 +3,76 @@ package nullpointer.json
 import nullpointer.json.JsonTokens._
 
 class JsonTokenizer {
+  import JsonTokenizer._
+
   def tokenize(source: String): Stream[JsonToken] =
-    Stream.iterate[(String, JsonToken)]((source, StartOfSourceToken)) {
-      case (sourceLeft, _) =>
+    Stream.iterate(FoundToken.startOfSource(source)) {
+      case FoundToken(sourceLeft, _) =>
         if (!sourceLeft.isEmpty)
           (sourceLeft #:: sourceLeft.tails.toStream)
             .map(findToken)
             .dropWhile(_.isEmpty)
             .map(_.get)
             .headOption
-            .getOrElse((sourceLeft, EndOfSourceToken))
+            .getOrElse(FoundToken.endOfSource)
         else
-          (sourceLeft, EndOfSourceToken)
-    }.tail.map(_._2)
+          FoundToken.endOfSource
+    }.tail.map(_.token)
 
-  private def findToken(source: String): Option[(String, JsonToken)] =
+  private def findToken(source: String): Option[FoundToken] =
     findNull(source)
       .orElse(findFalse(source))
       .orElse(findTrue(source))
       .orElse(findSimpleToken(source))
 
-  private def findNull(source: String): Option[(String, JsonToken)] = {
+  private def findNull(source: String): Option[FoundToken] = {
     if (source.length == 4 && source.take(4) == "null")
-      Some((source.drop(4), NullToken))
+      Some(FoundToken(source.drop(4), NullToken))
     else
       None
   }
 
-  private def findFalse(source: String): Option[(String, JsonToken)] = {
+  private def findFalse(source: String): Option[FoundToken] = {
     if (source.length == 5 && source.take(5) == "false")
-      Some((source.drop(5), FalseToken))
+      Some(FoundToken(source.drop(5), FalseToken))
     else
       None
   }
 
-  private def findTrue(source: String): Option[(String, JsonToken)] = {
+  private def findTrue(source: String): Option[FoundToken] = {
     if (source.length == 4 && source.take(4) == "true")
-      Some((source.drop(4), TrueToken))
+      Some(FoundToken(source.drop(4), TrueToken))
     else
       None
   }
 
-  private def findSimpleToken(source: String): Option[(String, JsonToken)] = {
+  private def findSimpleToken(source: String): Option[FoundToken] = {
     val sourceHead = source.head
     if (sourceHead == ':')
-      Some((source.tail, ColonToken))
+      Some(FoundToken(source.tail, ColonToken))
     else if (sourceHead == ',')
-      Some((source.tail, ComaToken))
+      Some(FoundToken(source.tail, ComaToken))
     else if (sourceHead == '[')
-      Some((source.tail, SquareBracketOpenToken))
+      Some(FoundToken(source.tail, SquareBracketOpenToken))
     else if (sourceHead == ']')
-      Some((source.tail, SquareBracketCloseToken))
+      Some(FoundToken(source.tail, SquareBracketCloseToken))
     else if (sourceHead == '{')
-      Some((source.tail, CurlyBracketOpenToken))
+      Some(FoundToken(source.tail, CurlyBracketOpenToken))
     else if (sourceHead == '}')
-      Some((source.tail, CurlyBracketCloseToken))
+      Some(FoundToken(source.tail, CurlyBracketCloseToken))
     else
       None
+  }
+}
+
+object JsonTokenizer {
+  case class FoundToken(sourceLeft: String, token: JsonToken)
+
+  object FoundToken {
+    def startOfSource(source: String): FoundToken =
+      FoundToken(source, StartOfSourceToken)
+
+    def endOfSource: FoundToken =
+      FoundToken("", EndOfSourceToken)
   }
 }
