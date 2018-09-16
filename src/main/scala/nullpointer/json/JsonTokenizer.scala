0aaa92df826bc1c0747c39f58cbbff252428fae3
vdrake6 <vdrake6@gmail.com>
2018-09-17 00:36:51 +0200

    JsonTokenizer refactor

--- src/main/scala/nullpointer/json/JsonTokenizer.scala
+++ src/main/scala/nullpointer/json/JsonTokenizer.scala
@@ -2,8 +2,6 @@ package nullpointer.json
 
 import nullpointer.json.JsonTokens._
 
-import scala.collection.immutable.WrappedString
-
 class JsonTokenizer {
   def tokenize(source: String): Stream[JsonToken] =
     Stream.iterate[(String, JsonToken)]((source, StartOfSourceToken)) {
@@ -19,15 +17,36 @@ class JsonTokenizer {
           (sourceLeft, EndOfSourceToken)
     }.tail.map(_._2)
 
-  private def findToken(source: String): Option[(String, JsonToken)] = {
-    val sourceHead = source.head
+  private def findToken(source: String): Option[(String, JsonToken)] =
+    findNull(source)
+      .orElse(findFalse(source))
+      .orElse(findTrue(source))
+      .orElse(findSimpleToken(source))
+
+  private def findNull(source: String): Option[(String, JsonToken)] = {
     if (source.length == 4 && source.take(4) == "null")
-      Some(source.drop(4), NullToken)
-    else if (source.length == 5 && source.take(5) == "false")
-      Some(source.drop(5), FalseToken)
-    else if (source.length == 4 && source.take(4) == "true")
-      Some(source.drop(4), TrueToken)
-    else if (sourceHead == ':')
+      Some((source.drop(4), NullToken))
+    else
+      None
+  }
+
+  private def findFalse(source: String): Option[(String, JsonToken)] = {
+    if (source.length == 5 && source.take(5) == "false")
+      Some((source.drop(5), FalseToken))
+    else
+      None
+  }
+
+  private def findTrue(source: String): Option[(String, JsonToken)] = {
+    if (source.length == 4 && source.take(4) == "true")
+      Some((source.drop(4), TrueToken))
+    else
+      None
+  }
+
+  private def findSimpleToken(source: String): Option[(String, JsonToken)] = {
+    val sourceHead = source.head
+    if (sourceHead == ':')
       Some((source.tail, ColonToken))
     else if (sourceHead == ',')
       Some((source.tail, ComaToken))
