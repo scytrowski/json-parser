9031797f1675a147b6fba45b9c75f7b6e9a87b71
vdrake6 <vdrake6@gmail.com>
2018-09-18 00:48:29 +0200

    Hexadecimal conversion moved to separate object

---
+++ src/main/scala/nullpointer/json/Hexadecimal.scala
@@ -0,0 +1,30 @@
+package nullpointer.json
+
+object Hexadecimal {
+  private lazy val hexCharValues: Map[Char, Byte] = Map(
+    '0' -> 0,
+    '1' -> 1,
+    '2' -> 2,
+    '3' -> 3,
+    '4' -> 4,
+    '5' -> 5,
+    '6' -> 6,
+    '7' -> 7,
+    '8' -> 8,
+    '9' -> 9,
+    'a' -> 10,
+    'A' -> 10,
+    'b' -> 11,
+    'B' -> 11,
+    'c' -> 12,
+    'C' -> 12,
+    'd' -> 13,
+    'D' -> 13,
+    'e' -> 14,
+    'E' -> 14,
+    'f' -> 15,
+    'F' -> 15
+  )
+
+  def valueOf(hexChar: Char): Option[Byte] = hexCharValues.get(hexChar)
+}
---
+++ src/test/scala/nullpointer/json/HexadecimalSpec.scala
@@ -0,0 +1,63 @@
+package nullpointer.json
+
+import org.scalatest.prop.TableDrivenPropertyChecks._
+
+class HexadecimalSpec extends CommonSpec {
+  import HexadecimalSpec._
+
+  describe("Hexadecimal") {
+    val validHexadecimalCharCases = Table(
+      ("validHexadecimalChar", "expectedValue"),
+      validHexadecimalChars.toSeq:_*
+    )
+    forAll(validHexadecimalCharCases) { (validHexadecimalChar, expectedValue) =>
+      it(s"must return $expectedValue when char is '$validHexadecimalChar'") {
+        val result = Hexadecimal.valueOf(validHexadecimalChar)
+        result.isDefined mustBe true
+        result.get mustBe expectedValue
+      }
+    }
+
+    it("must return None when char is not valid hexadecimal character") {
+      val invalidHexadecimalCharCases = Table(
+        "invalidHexadecimalChar",
+        invalidHexadecimalChars: _*
+      )
+      forAll(invalidHexadecimalCharCases) { invalidHexadecimalChar =>
+        val result = Hexadecimal.valueOf(invalidHexadecimalChar)
+        result.isEmpty mustBe true
+      }
+    }
+  }
+}
+
+private object HexadecimalSpec {
+  private lazy val validHexadecimalChars: Map[Char, Byte] = Map(
+    '0' -> 0,
+    '1' -> 1,
+    '2' -> 2,
+    '3' -> 3,
+    '4' -> 4,
+    '5' -> 5,
+    '6' -> 6,
+    '7' -> 7,
+    '8' -> 8,
+    '9' -> 9,
+    'a' -> 10,
+    'A' -> 10,
+    'b' -> 11,
+    'B' -> 11,
+    'c' -> 12,
+    'C' -> 12,
+    'd' -> 13,
+    'D' -> 13,
+    'e' -> 14,
+    'E' -> 14,
+    'f' -> 15,
+    'F' -> 15
+  )
+
+  private lazy val invalidHexadecimalChars: Seq[Char] = (0 until 256)
+    .map(_.toChar)
+    .diff(validHexadecimalChars.keys.toSeq)
+}
