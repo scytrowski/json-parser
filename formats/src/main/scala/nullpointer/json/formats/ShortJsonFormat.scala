992d5d95970cdd7a4d6fa770ca4d92a6031c7ace
vdrake6 <vdrake6@gmail.com>
2018-09-25 23:17:57 +0200

    ShortJsonFormat implementation

---
+++ formats/src/main/scala/nullpointer/json/formats/ShortJsonFormat.scala
@@ -0,0 +1,16 @@
+package nullpointer.json.formats
+
+import nullpointer.json.JsonFormat
+import nullpointer.json.JsonValues.{JsonNumber, JsonValue}
+import nullpointer.json.formats.JsonFormatExceptions.JsonDeserializationException
+
+import scala.util.{Failure, Success, Try}
+
+object ShortJsonFormat extends JsonFormat[Short] {
+  override def serialize(obj: Short): Try[JsonValue] = Success(JsonNumber(obj))
+
+  override def deserialize(json: JsonValue): Try[Short] = json match {
+    case JsonNumber(value) => Success(value.toShort)
+    case _ => Failure(JsonDeserializationException(s"$json cannot be deserialized as Short"))
+  }
+}
---
+++ formats/src/test/scala/nullpointer/json/formats/ShortJsonFormatSpec.scala
@@ -0,0 +1,63 @@
+package nullpointer.json.formats
+
+import nullpointer.json.JsonValues._
+import nullpointer.json.formats.JsonFormatExceptions.JsonDeserializationException
+import nullpointer.json.testing.{CommonSpec, TryMatchers}
+import org.scalatest.prop.TableDrivenPropertyChecks._
+
+import scala.util.Random
+
+class ShortJsonFormatSpec extends CommonSpec with TryMatchers {
+  import ShortJsonFormatSpec._
+
+  describe("A ShortJsonFormat") {
+    it("must serialize to JsonNumber with correct value") {
+      val serializeShortTestCases = Table(
+        ("number", "expectedValue"),
+        (1 to 100)
+          .map(_ => nextShort)
+          .map(n => n -> JsonNumber(n)):_*
+      )
+      forAll(serializeShortTestCases) { (number, expectedValue) =>
+        val result = ShortJsonFormat.serialize(number)
+        result must succeedWith[JsonValue](expectedValue)
+      }
+    }
+
+    it("must deserialize JsonNumber to correct Short") {
+      val deserializeJsonNumberTestCases = Table(
+        ("json", "expectedNumber"),
+        (1 to 100)
+          .map(_ => Random.nextDouble)
+          .map(n => JsonNumber(n) -> n.toShort):_*
+      )
+      forAll(deserializeJsonNumberTestCases) { (json, expectedNumber) =>
+        val result = ShortJsonFormat.deserialize(json)
+        result must succeedWith(expectedNumber)
+      }
+    }
+
+    it("must fail with JsonDeserializationException on deserialization when JSON is not JsonNumber") {
+      val failingDeserializationTestCases = Table(
+        "json",
+        JsonNull,
+        JsonBoolean(false),
+        JsonBoolean(true),
+        JsonString("abc"),
+        JsonArray(JsonBoolean(true), JsonNull),
+        JsonObject("a" -> JsonNull, "b" -> JsonBoolean(false))
+      )
+      forAll(failingDeserializationTestCases) { json =>
+        val result = ShortJsonFormat.deserialize(json)
+        result must failWith[JsonDeserializationException]
+      }
+    }
+  }
+}
+
+private object ShortJsonFormatSpec {
+  private lazy val nextShortUpperBound: Int = Short.MaxValue + Short.MinValue.abs
+
+  def nextShort: Short =
+    (Random.nextInt(nextShortUpperBound) - Short.MinValue.abs).toShort
+}
